5c2d9d5d8d4fc2543c2d36dbcbc925b187f4cbcb
TANABE Ken-ichi <nabeken@tknetworks.org>
2015-08-12 21:10:43 +0900

    bumped to v0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'nabeken@tknetworks.org'
 license          'apache2'
 description      'Installs/Configures docker_deploy'
 long_description 'Installs/Configures docker_deploy'
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'docker'
