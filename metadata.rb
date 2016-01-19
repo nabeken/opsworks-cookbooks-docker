de52e15907e50e7837cdae50389a23e483fb9bd6
TANABE Ken-ichi <nabeken@tknetworks.org>
2016-01-19 19:59:57 +0900

    Move docker cookbook to ~2.4

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures docker_deploy'
 long_description 'Installs/Configures docker_deploy'
 version          '0.2.0'
 
-depends 'docker', '~> 2.3.5'
+depends 'docker', '~> 2.4'
